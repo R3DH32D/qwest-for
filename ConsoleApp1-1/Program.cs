b40e0c74fb1897a0c15187ef5a2533264b3dc23f
t4 43r <troltoh@gmail.com>
2022-09-24 10:33:23 +0800

    Добавьте файлы проекта.

---
+++ ConsoleApp1-1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApplication
+{
+    public class Logic
+    {
+        public static string Count(double setTable)
+        {
+            string outMessage = "";
+
+            for (int i = 1; i < 10; i++)
+            {
+                if ((setTable <= 9) && (setTable >= 1))
+                {
+                    var countTable = i * setTable;
+                    outMessage = outMessage  + i + " * " + setTable + " = " + countTable+ "\n";
+                }
+                else
+                {
+                    outMessage = "Введено неверное число, перезапустите программу и повторите попытку";
+                }
+               
+
+
+            }
+            return outMessage;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число, по которому надо построить таблицу умножения-");
+
+            double setTable = double.Parse(Console.ReadLine());
+
+            var outMessage = Logic.Count(setTable);
+            Console.WriteLine(outMessage);
+
+
+        }
+    }
+}
+
+
+
---
+++ ConsoleApp1-1Tests/LogicTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FirstApplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApplication.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void CountTest1()
+        {
+
+            var message = Logic.Count(2);
+            Assert.AreEqual("1 * 2 = 2\n"+ "2 * 2 = 4\n"+ "3 * 2 = 6\n"+ "4 * 2 = 8\n"+ "5 * 2 = 10\n" + "6 * 2 = 12\n" + "7 * 2 = 14\n" + "8 * 2 = 16\n" + "9 * 2 = 18\n" , message);
+        }
+        [TestMethod()]
+        public void CountTest2()
+        {
+
+            var message = Logic.Count(15);
+            Assert.AreSame("Введено неверное число, перезапустите программу и повторите попытку", message);
+        }
+        
+        
+    }
+}
